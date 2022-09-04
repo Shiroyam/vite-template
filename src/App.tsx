d4226e6a205733edef8c42b39de9a45d461d8930
Semyon Zasov <semenmaslina@gmail.com>
2022-09-04 20:37:49 +0300

    test 2

--- src/App.tsx
+++ src/App.tsx
@@ -12,7 +12,7 @@ function App() {
           <img src="/vite.svg" className="logo" alt="Vite logo" />
         </a>
         <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
+                <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
       <h1>Vite + React</h1>
